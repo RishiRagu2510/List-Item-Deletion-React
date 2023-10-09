d034983d41ecf41bc985a00959c9d81c294ba438
RishiRagu2510 <rishiragu25@gmail.com>
2023-10-09 11:26:10 +0530

    DeletableList is updated

---
+++ listitemdeletion/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Deletablelist from './Components/Deletablelist';
+
+function App() {
+  return (
+    <div>
+    <Deletablelist/>
+    </div>
+  );
+}
+
+export default App;
